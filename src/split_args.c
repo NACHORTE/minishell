86ccd8edf00ca0633ad5146773e244a7b44fb116
shuoscaryo <shuoscaryo@gmail.com>
2023-09-09 23:40:19 +0200

    norminette and commented functions

--- src/split_args.c
+++ src/split_args.c
@@ -6,167 +6,16 @@
 /*   By: orudek <orudek@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 14:28:51 by orudek            #+#    #+#             */
-/*   Updated: 2023/09/09 23:22:17 by orudek           ###   ########.fr       */
+/*   Updated: 2023/09/09 23:39:47 by orudek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*dup_str(char *out, const char *s, int len, char c)
-{
-	int	i;
-	int	state;
-
-	state = 0;
-	i = 0;
-	s--;
-	while(i < len && *++s)
-	{
-		if (state == 0 && *s == '\'' || state == 0 && *s == '"')
-		{
-			state = (*s == '\'') * 2 + (*s == '"');
-			continue ;
-		}
-		if (state == 1 && *s == '"' || state == 2 && *s == '\'')
-		{
-			state = 0;
-			continue ;
-		}
-		out[i++] = *s;
-	}
-	return (out);
-}
-
-static char *dup_redirection(char *out, const char *s, int len, char c)
-{
-	int i;
-	int j;
-
-	out[0] = *s++;
-	i = 1;
-	if (*s == out[0])
-		out[i++] = *s++;
-	else if (*s == '<' || *s == '>')
-		*s++;
-	while (*s == c)
-		s++;
-	if (*s == '<' || *s == '>')
-		return (out);
-	(void)dup_str(&out[i], s, len - i, c);
-	return (out);
-}
-
-/*	get_str_len:
-		Auxiliary function used in "get_word" and "get_num_words"
-		Counts the length of the word ignoring quotes that surround strings,
-		and ending when a delimiter or '<' '>' is found outside quotes.
-		If quotes are besides more non delimiter characters, it still counts as
-		the same word.
-	Parameters:
-		s: string from which to count the length of the word.
-		c: character used as the delimiter between words.
-		len: reference to the variable where the length of the word will be
-			stored (length of the characters without the ignored ones)
-		i: reference to the variable where the total length of the word will be
-			store (lenght counting ignored characters)
-	Return:
-		The return is done in "len" and "i" variables.
-	Code:
-		Iterates the "s" string using "i" until its the end of the string, or a
-		delimiter character or '<' '>' is found outside quotes.
-		The loop works with states:
-			-state 0: Means the read characters are outside quotes
-			-state 1: Means the read characters are inside "" quotes
-			-state 2: Means the read characters are inside '' quotes
-		(state 1 and 2 are different because there can be opposite quotes
-		inside)
-		When in state 0, if any quote is found the state will change. The
-		assignation is done like that to shorten the code. if s[i]=' the result
-		is 2 and if s[i]=" the result is 1
-		When in either state 1 or 2, if the same quotation type is found, state
-		goes back to 0.
-		In both state transitions, the iteration is skipped so the surrounding
-		quotation characters don't get counted in the length.
-		With every successful iteration, the length of the word is increased by
-		one.
-*/
-static void	get_str_len(const char *s, char c, int *len, int *i)
-{
-	int state = 0;
-
-	state = 0;
-	*len = 0;
-	*i = -1;
-	while(s[++(*i)] && (state || (s[*i] != c && s[*i] != '<' && s[*i] != '>')))
-	{
-		if (state == 0 && (s[*i] == '\'' || s[*i] == '"'))
-		{
-			state = (s[*i] == '\'') * 2 + (s[*i] == '"');
-			continue ;
-		}
-		if (state == 1 && s[*i] == '"' || state == 2 && s[*i] == '\'')
-		{
-			state = 0;
-			continue ;
-		}
-		(*len)++;
-	}
-}
-
-/*	get_redirection_len:
-		Auxiliary function used in "get_word" and "get_num_words"
-		Counts the length of the next word of the string assuming it will have
-		this format:
-			<[<][spaces][name]
-		The first character is supossed to be '<' or '>'
-		The second character can be a '<' or '>', but it will only be counted
-		in the length if it is the same as the first one.
-		After the delimiter there can be [0-n] delimiter characters that will
-		not be counted in the length.
-		The name is gonna be treated a string, allowing "" or '', and
-		ending if it finds '<' or '>'. The quotes surrounding a string won't be
-		counted in the length.
-	Parameters:
-		s: string from which to count the length of the word.
-		c: character used as the delimiter between words.
-		len: reference to the variable where the length of the word will be
-			stored (length of the characters without the ignored ones)
-		i: reference to the variable where the total length of the word will be
-			store (lenght counting ignored characters)
-	Return:
-		The return is done in "len" and "i" variables.
-	Code:
-		First sets "len" and "i" to 1 because the first character is '<'.
-		Then if the second one is the same as the first, adds 1 to both
-		variables.
-		If the second variable is a '<' but not the same type, don't count it
-		in "len" but still increase "i"
-		Afterwards, increase i with as many delimiter characters there are
-		until a word or '\0' is found.
-		Then call the "get_str_len" function to get the lenght of the string
-		add the length returned from the function to "len" and "i" to get the
-		total length.
-*/
-static void	get_redirection_len(const char *s, char c, int *len, int *i)
-{
-	int str_len;
-	int str_i;
-
-	*i = 1;
-	*len = 1;
-	if (s[1] == s[0])
-	{
-		(*i)++;
-		(*len)++;
-	}
-	else if (s[1] == '<' || s[1] == '>')
-		(*i)++;
-	while(s[*i] == c)
-		(*i)++;
-	(void)get_str_len(&s[*i], c, &str_len, &str_i);
-	*i += str_i;
-	*len += str_len;
-}
+char	*dup_str(char *out, const char *s, int len, char c);
+void	dup_redirection(char *out, const char *s, int len, char c);
+void	get_str_len(const char *s, char c, int *len, int *i);
+void	get_redirection_len(const char *s, char c, int *len, int *i);
 
 /*	get_word:
 		Auxiliary function used in "split_args".
@@ -311,11 +160,11 @@ char	**split_args(char const *s, char c)
 	return (words);
 }
 
-
+/*
 int main(int c, char **v)
 {
 	char **s = split_args(v[1], ' ');
 	for (int i = 0; s[i]; i++)
 		printf("str[%d]=\'%s\'\n",i,s[i]);
 	return (1);
-}
+}*/
