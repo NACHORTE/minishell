a63b535054bf9ee5089bc4e1a8469d67a21990b4
shuoscaryo <shuoscaryo@gmail.com>
2023-09-09 17:44:01 +0200

    Modified split args so it handles "", '', <, >
    It's still a preliminary version, if the string does "< < > outfile"
    it outputs "<<>outfile". It doesn't consider '<<' or '>>', only reads and saves
    '<' '>' symbols until a string is found and then saves it too.

--- src/ft_split_args.c
+++ src/ft_split_args.c
@@ -3,157 +3,218 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split_args.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: iortega- <iortega-@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: orudek <orudek@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 14:28:51 by orudek            #+#    #+#             */
-/*   Updated: 2023/09/05 22:47:08 by iortega-         ###   ########.fr       */
+/*   Updated: 2023/09/09 17:41:02 by orudek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
-/*static int	ft_get_num_words(const char *s, char c)
-{
-	size_t	count;
-	size_t	is_word;
-	size_t	comma;
+#include "libft.h"
+static void	get_redirection_len(const char *s, char c, int *len, int *i);
+static void	get_str_len(const char *s, char c, int *len, int *i);
+static char	*dup_str(char *out, const char *s, int len, char c);
 
-	comma = 0;
-	is_word = 0;
-	count = 0;
+static int	get_num_words(const char *s, char c)
+{
+	int	count;
+	int	len;
+	int	i;
+
 	while (*s)
 	{
-		if (*s == '"')
-		{
-			if (!comma)
-			{
-				comma = 1;
-				if (!is_word)
-					count++;
-			}
-			else if (comma)
-				comma = 0;
-		}
-		if (is_word && *s == c)
-			is_word = 0;
-		else if (!is_word && *s != c)
-		{
-			is_word = 1;
-			if (!comma)
-				count++;
-		}
-		s++;
-	}
-	printf("%ld\n", count);
-	return (count);
-}
-*/
-static int	ft_get_num_words(const char *s, char c)
-{
-	size_t	count;
-	size_t	is_word;
-	size_t	flag;
-
-	flag = 0;
-	is_word = 0;
-	count = 0;
-	while (*s)
-	{
-		if (*s == '"')
-			flag = !flag;
-		if (!is_word && *s != c)
-		{
-			is_word = 1;
+		i = 0;
+		while (*s == c)
+			s++;
+		//get the lenght of the word found
+		if (*s == '>' || *s == '<')
+			get_redirection_len(s, c, &len, &i);
+		else
+			get_str_len(s, c, &len, &i);
+		if (i)
 			count++;
-		}
-		else if (is_word && !flag && *s == c)
-			is_word = 0;
-		s++;
+		s += i;
 	}
+	printf("number of words: %d\n",count);
 	return (count);
 }
 
-static char	*ft_get_word(const char **s, char c)
+static char *dup_redirection(char *out, const char *s, int len, char c)
 {
-	size_t	i;
-	char	*out;
-	size_t	len;
-	int		flag;
+	int i;
 
-	//skip all spaces
-	while (**s == c)
-		(*s)++;
-	//get amount of bytes of string
+	//save all the spaces and << < > >> until a letter is found
+	//this parse allows "<< < > hello" to be considered a word
 	i = 0;
-	len = 0;
-	flag = 0;
-	while ((*s)[i] && ((*s)[i] != c || flag)) // while string is not over, and character is not space outside quotation marks pair
+	while(*s == c || *s == '<' || *s == '>')
 	{
-		if ((*s)[i++] == '"')
-			flag = !flag;
-		else //don't count "
-			len++;
+		if (*s != c)
+			out[i++] = *s;
+		s++;
 	}
-	out = malloc(len + 1);
-	if (out == NULL)
-		return (NULL);
-	i = 0;
-	while (i < len)
-	{
-		if (*(*s) == '"')
-			(*s)++;
-		out[i++] = *((*s)++);
-	}
-	if (*(*s) == '"')
-		(*s)++;
-	out[i] = 0;
+	(void)dup_str(&out[i], s, len - i, c);
 	return (out);
 }
 
-static char	**ft_free(char **words)
+static char	*dup_str(char *out, const char *s, int len, char c)
 {
-	size_t	i;
+	int	i;
+	int	state;
 
+	state = 0;
 	i = 0;
-	while (words[i])
-		free(words[i++]);
-	free (words);
-	return (NULL);
+	s--;  //go back 1 character so i can do ++s in the while condition
+	printf("\tDUP_STR: s= ||%s||\n", s+1);
+	while(i < len && *++s)
+	{
+		if (state == 0 && *s == '\'' || state == 0 && *s == '"')
+		{
+			state = (*s == '\'') * 2 + (*s == '"');
+			continue ;
+		}
+		if (state == 1 && *s == '"' || state == 2 && *s == '\'')
+		{
+			state = 0;
+			continue ;
+		}
+		printf("\tDUP_STR: state=%d, *s: %c\n",state,*s);
+		out[i++] = *s;
+	}
+	return (out);
 }
 
-char	**ft_split_args(char const *s, char c)
+static char	*dup_word(const char *s, int len, char c)
+{
+	char	*out;
+	int		i;
+
+	//allocate memory for the new string
+	out = malloc(len + 1);
+	if (out == NULL)
+		return (NULL);
+	if (*s == '<' || *s == '>')
+	{
+		printf("DUP_WORD: calling DUP_REDIRECTION\n");
+		return (dup_redirection(out, s, len, c));
+	}
+	else
+	{
+		printf("DUP_WORD: calling DUP_STR\n");
+		return (dup_str(out, s, len, c));
+	}
+}
+
+static void	get_redirection_len(const char *s, char c, int *len, int *i)
+{
+	int str_len;
+	int str_i;
+
+	*i = 0;
+	*len = 0;
+	while(s[*i] == c || s[*i] == '<' || s[*i] == '>')
+	{
+		if (s[*i] != c)
+			(*len)++;
+		(*i)++;
+	}
+	(void)get_str_len(&s[*i], c, &str_len, &str_i);
+	*i += str_i;
+	*len += str_len;
+}
+
+static void	get_str_len(const char *s, char c, int *len, int *i)
+{
+	int state = 0;
+
+	state = 0;
+	*len = 0;
+	*i = -1;
+	while(s[++(*i)] && (state || (s[*i] != c && s[*i] != '<' && s[*i] != '>')))
+	{
+		if (state == 0 && (s[*i] == '\'' || s[*i] == '"'))
+		{
+			//if s == ' -> state = 1 else state = 2
+			state = (s[*i] == '\'') * 2 + (s[*i] == '"');
+			continue ;
+		}
+		if (state == 1 && s[*i] == '"' || state == 2 && s[*i] == '\'')
+		{
+			state = 0;
+			continue ;
+		}
+		(*len)++;
+	}
+}
+
+static char	*get_word(const char **s, char c)
+{
+	int		len;
+	int		i;
+	char	*word;
+
+	//skip all spaces
+	printf("GETWORD: string before skipping spaces: ||%s||\n",*s);
+	while (**s == c)
+		(*s)++;
+	printf("GETWORD: string after skipping spaces: ||%s||\n",*s);
+	//get the lenght of the word found
+	if (**s == '>' || **s == '<')
+	{
+		get_redirection_len(*s, c, &len, &i);
+		printf("GETWORD: string len from GET_REDIRECTION_LEN: len:%d, i:%d\n", len, i);
+	}
+	else
+	{
+		get_str_len(*s, c, &len, &i);
+		printf("GETWORD: string len from GET_STR_LEN: len:%d, i:%d\n", len, i);
+	}
+	//get a new string with the word saved
+	word = dup_word(*s, len, c);
+	printf("GETWORD: word saved as: ||%s||\n",word);
+	if (!word)
+		return (NULL);
+	//advance the pointer to the end of the word
+	*s += i;
+	printf("GETWORD: string after getting advanced %d spaces : ||%s||\n", i, *s);
+	//return a new string with the word (ignoring "", '', or spaces after < >)
+	return (word);
+}
+
+char	**split_args(char const *s, char c)
 {
 	char	**words;
 	size_t	num_words;
 	size_t	i;
 
-	num_words = ft_get_num_words(s, c);
+	num_words = get_num_words(s, c);
 	words = malloc((sizeof(char *) * (num_words + 1)));
 	if (words == NULL)
 		return (NULL);
 	i = 0;
 	while (i < num_words)
 	{
-		words[i] = ft_get_word(&s, c);
+		words[i] = get_word(&s, c);
 		if (!words[i])
-			return (ft_free(words));
+			return ((char **)ft_array_free(words));
 		i++;
 	}
 	words[i] = 0;
 	return (words);
 }
-/*
+
 int main(int c, char **v)
 {
-	char *donut = "hola a\"qu  e tal guapisimo     ignacio\"   a     ";
 	char **prueba;
-	printf("%s\n",donut);
-	prueba = ft_split(donut, ' ');
+	if (c != 2)
+		return 1;
+	printf("printf:\"%s\"\n",v[1]);
+	prueba = split_args(v[1], ' ');
 	int i = 0;
 	while (prueba[i])
 		printf("%s$\n", prueba[i++]);
 	return 0;
 }
-
+/*
 int main()
 {
     char *cadenita = "^^^1^^2a,^^^^3^^^^--h^^^^";
