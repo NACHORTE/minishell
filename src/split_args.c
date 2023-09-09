8b472acce6f4c7eb054312b69a532821e9a7b3dd
shuoscaryo <shuoscaryo@gmail.com>
2023-09-09 18:37:42 +0200

    Updated split args function so it works better with << >> < >
    The function now works like the terminal and if it finds "<> makefile"
    it converts it to "<makefile", also if "< <file" it does "<" "<file".

--- src/split_args.c
+++ src/split_args.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_split_args.c                                    :+:      :+:    :+:   */
+/*   split_args.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: orudek <orudek@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 14:28:51 by orudek            #+#    #+#             */
-/*   Updated: 2023/09/09 17:41:02 by orudek           ###   ########.fr       */
+/*   Updated: 2023/09/09 18:37:29 by orudek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,23 +35,27 @@ static int	get_num_words(const char *s, char c)
 			count++;
 		s += i;
 	}
-	printf("number of words: %d\n",count);
+	//printf("number of words: %d\n",count);
 	return (count);
 }
 
 static char *dup_redirection(char *out, const char *s, int len, char c)
 {
 	int i;
+	int j;
 
 	//save all the spaces and << < > >> until a letter is found
 	//this parse allows "<< < > hello" to be considered a word
-	i = 0;
-	while(*s == c || *s == '<' || *s == '>')
-	{
-		if (*s != c)
-			out[i++] = *s;
+	out[0] = *s++;
+	i = 1;
+	if (*s == out[0])
+		out[i++] = *s++;
+	else if (*s == '<' || *s == '>')
+		*s++;
+	while (*s == c)
 		s++;
-	}
+	if (*s == '<' || *s == '>')
+		return (out);
 	(void)dup_str(&out[i], s, len - i, c);
 	return (out);
 }
@@ -64,7 +68,7 @@ static char	*dup_str(char *out, const char *s, int len, char c)
 	state = 0;
 	i = 0;
 	s--;  //go back 1 character so i can do ++s in the while condition
-	printf("\tDUP_STR: s= ||%s||\n", s+1);
+	//printf("\tDUP_STR: len= %d, s= ||%s||\n",len, s+1);
 	while(i < len && *++s)
 	{
 		if (state == 0 && *s == '\'' || state == 0 && *s == '"')
@@ -77,7 +81,7 @@ static char	*dup_str(char *out, const char *s, int len, char c)
 			state = 0;
 			continue ;
 		}
-		printf("\tDUP_STR: state=%d, *s: %c\n",state,*s);
+		//printf("\tDUP_STR: state=%d, *s: %c\n",state,*s);
 		out[i++] = *s;
 	}
 	return (out);
@@ -94,12 +98,12 @@ static char	*dup_word(const char *s, int len, char c)
 		return (NULL);
 	if (*s == '<' || *s == '>')
 	{
-		printf("DUP_WORD: calling DUP_REDIRECTION\n");
+		//printf("DUP_WORD: calling DUP_REDIRECTION\n");
 		return (dup_redirection(out, s, len, c));
 	}
 	else
 	{
-		printf("DUP_WORD: calling DUP_STR\n");
+		//printf("DUP_WORD: calling DUP_STR\n");
 		return (dup_str(out, s, len, c));
 	}
 }
@@ -109,14 +113,19 @@ static void	get_redirection_len(const char *s, char c, int *len, int *i)
 	int str_len;
 	int str_i;
 
-	*i = 0;
-	*len = 0;
-	while(s[*i] == c || s[*i] == '<' || s[*i] == '>')
+	*i = 1;
+	*len = 1;
+	if (s[1] == s[0])
 	{
-		if (s[*i] != c)
-			(*len)++;
 		(*i)++;
+		(*len)++;
 	}
+	else if (s[1] == '<' || s[1] == '>')
+		(*i)++;
+	while(s[*i] == c)
+		(*i)++;
+	if (s[*i] == '<' || s[*i] == '>')
+		return ;
 	(void)get_str_len(&s[*i], c, &str_len, &str_i);
 	*i += str_i;
 	*len += str_len;
@@ -153,29 +162,28 @@ static char	*get_word(const char **s, char c)
 	char	*word;
 
 	//skip all spaces
-	printf("GETWORD: string before skipping spaces: ||%s||\n",*s);
 	while (**s == c)
 		(*s)++;
-	printf("GETWORD: string after skipping spaces: ||%s||\n",*s);
+	//printf("GETWORD: string after skipping spaces: ||%s||\n",*s);
 	//get the lenght of the word found
 	if (**s == '>' || **s == '<')
 	{
 		get_redirection_len(*s, c, &len, &i);
-		printf("GETWORD: string len from GET_REDIRECTION_LEN: len:%d, i:%d\n", len, i);
+		//printf("GETWORD: string len from GET_REDIRECTION_LEN: len:%d, i:%d\n", len, i);
 	}
 	else
 	{
 		get_str_len(*s, c, &len, &i);
-		printf("GETWORD: string len from GET_STR_LEN: len:%d, i:%d\n", len, i);
+		//printf("GETWORD: string len from GET_STR_LEN: len:%d, i:%d\n", len, i);
 	}
 	//get a new string with the word saved
 	word = dup_word(*s, len, c);
-	printf("GETWORD: word saved as: ||%s||\n",word);
+	//printf("GETWORD: word saved as: ||%s||\n",word);
 	if (!word)
 		return (NULL);
 	//advance the pointer to the end of the word
 	*s += i;
-	printf("GETWORD: string after getting advanced %d spaces : ||%s||\n", i, *s);
+	//printf("GETWORD: string after getting advanced %d spaces : ||%s||\n", i, *s);
 	//return a new string with the word (ignoring "", '', or spaces after < >)
 	return (word);
 }
@@ -202,7 +210,7 @@ char	**split_args(char const *s, char c)
 	return (words);
 }
 
-int main(int c, char **v)
+/*int main(int c, char **v)
 {
 	char **prueba;
 	if (c != 2)
@@ -214,16 +222,4 @@ int main(int c, char **v)
 		printf("%s$\n", prueba[i++]);
 	return 0;
 }
-/*
-int main()
-{
-    char *cadenita = "^^^1^^2a,^^^^3^^^^--h^^^^";
-    char **out = ft_split(cadenita,'^');
-    while(*out)
-	{
-        printf("%s %p$\n", *out, *out);
-		out++;
-	}
-	printf("%p$\n",*out);
-	return (0);
-}*/
+*/
