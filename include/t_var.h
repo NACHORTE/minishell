7a55edaed9e8c6ab879e96d19261b21b5bc15db8
shuoscaryo <shuoscaryo@gmail.com>
2023-09-21 20:36:49 +0200

    updated t_var header file

--- include/t_var.h
+++ include/t_var.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   t_var.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oscar <oscar@student.42.fr>                +#+  +:+       +#+        */
+/*   By: orudek <orudek@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 13:20:58 by orudek            #+#    #+#             */
-/*   Updated: 2023/09/20 22:37:24 by oscar            ###   ########.fr       */
+/*   Updated: 2023/09/21 17:03:17 by orudek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,12 @@ typedef struct s_variable
 	char	*content;
 }	t_var;
 
-//t_var *new_var(char *name, char *content);
 char	set_variable(t_list **lst, char *name, char *content);
 char	*get_variable(t_list *lst, char *name);
 void	unset_variable(t_list **lst, char *name);
-t_list	*array_to_var(char **array);
-char	**var_to_array(t_list *lst);
+t_list	*array_to_varlist(char **array);
+char	**varlist_to_array(t_list *lst);
 void	free_var(void *var);
+void	print_varlist(t_list *varlist); //XXX delete this before submitting project
 
 #endif
