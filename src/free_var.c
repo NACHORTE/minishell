134a74fd7738813ca290c96836798172154155b2
shuoscaryo <shuoscaryo@gmail.com>
2023-09-21 20:46:46 +0200

    modified free var so it doesn't give double free errors

--- src/free_var.c
+++ src/free_var.c
@@ -6,7 +6,7 @@
 /*   By: orudek <orudek@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 13:44:41 by orudek            #+#    #+#             */
-/*   Updated: 2023/09/11 16:02:07 by orudek           ###   ########.fr       */
+/*   Updated: 2023/09/21 16:42:41 by orudek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,11 @@
 
 void	free_var(void *var)
 {
-	free(((t_var *)var)->content);
-	free(((t_var *)var)->name);
+	if (!var)
+		return ;
+	if (((t_var *)var)->content)
+		free(((t_var *)var)->content);
+	if (((t_var *)var)->name)
+		free(((t_var *)var)->name);
 	free(((t_var *)var));
 }
