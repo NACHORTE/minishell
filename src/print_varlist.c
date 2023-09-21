561bc4888c10385341bb5014a0df8c601918ea2d
shuoscaryo <shuoscaryo@gmail.com>
2023-09-21 20:37:57 +0200

    Finished src/print_varlist.c
    The function prints everything inside a list containing variables, separated
    between name and content strings.

---
+++ src/print_varlist.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_varlist.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: orudek <orudek@student.42madrid.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/21 16:58:17 by orudek            #+#    #+#             */
+/*   Updated: 2023/09/21 17:32:34 by orudek           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "t_var.h"
+
+// Text attributes
+#define RESET "\x1b[0m"      // Reset all attributes to default
+#define BOLD "\x1b[1m"       // Bold or increased intensity
+#define DIM "\x1b[2m"        // Dim (decreased intensity)
+#define UNDERLINE "\x1b[4m"  // Underline
+#define BLINK "\x1b[5m"      // Blink
+#define REVERSE "\x1b[7m"    // Reverse (swap foreground and background colors)
+#define HIDDEN "\x1b[8m"     // Hidden (not widely supported)
+
+// Text colors
+#define BLACK "\x1b[30m"     	// Black text
+#define RED "\x1b[31m"       	// Red text
+#define GREEN "\x1b[32m"     	// Green text
+#define YELLOW "\x1b[33m"    	// Yellow text
+#define BLUE "\x1b[34m"      	// Blue text
+#define MAGENTA "\x1b[35m"   	// Magenta text
+#define CYAN "\x1b[36m"      	// Cyan text
+#define WHITE "\x1b[37m"     	// White text
+#define ORANGE "\x1b[38;5;208m"	// Orange text
+
+// Background colors
+#define BG_BLACK "\x1b[40m"  // Black background
+#define BG_RED "\x1b[41m"    // Red background
+#define BG_GREEN "\x1b[42m"  // Green background
+#define BG_YELLOW "\x1b[43m" // Yellow background
+#define BG_BLUE "\x1b[44m"   // Blue background
+#define BG_MAGENTA "\x1b[45m" // Magenta background
+#define BG_CYAN "\x1b[46m"   // Cyan background
+#define BG_WHITE "\x1b[47m"  // White background
+
+void 	print_varlist(t_list *varlist)
+{
+	int j;
+
+	j = 0;
+	while (varlist)
+	{
+		printf(BLUE"List %d: "CYAN"(%p)"BLUE", next "CYAN"(%p)\n"RESET, j++, varlist, varlist->next);
+		printf(GREEN"\tname = "RESET"%s\n", ((t_var *)varlist->content)->name);
+		printf(GREEN"\tcontent = "RESET"%s\n", ((t_var *)varlist->content)->content);
+		varlist = varlist->next;
+	}
+	printf("list %d: (%p)\n", j, varlist);
+}
