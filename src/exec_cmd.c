dc7e9addbfbb68a03bfcfb88efa17f65eac840b0
shuoscaryo <shuoscaryo@gmail.com>
2023-09-22 21:39:21 +0200

    Test version of exec_cmd, with an aux child function.
    The exec_cmd creates correctly the pipes and forks, linking them together.
    An auxiliary child has been made that only prints the infile and outfile.

--- src/exec_cmd.c
+++ src/exec_cmd.c
@@ -3,80 +3,124 @@
 /*                                                        :::      ::::::::   */
 /*   exec_cmd.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oscar <oscar@student.42.fr>                +#+  +:+       +#+        */
+/*   By: orudek <orudek@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 13:56:41 by oscar             #+#    #+#             */
-/*   Updated: 2023/09/20 22:33:15 by oscar            ###   ########.fr       */
+/*   Updated: 2023/09/22 21:35:07 by orudek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+#include <sys/types.h>	//XXX
+#include <sys/wait.h>	//XXX
+#include <semaphore.h>
+#include <fcntl.h>
 
-
+void	child(int infile, int outfile, char **cmd, char **env, sem_t *sem)
+{
+	sem_wait(sem);
+	printf("CHILD:\n\tinfile=%d\n\toutfile=%d\n", infile, outfile);
+	sem_post(sem);
+	sem_close(sem);
+	exit (0);
+}
 
 /*  If the command given is a builtin, then no fork must be made and executes
     the function for builtins. If it is a normal command, creates a fork and
     calls the child process setting infile=stdin, outfile=stdout
     If the command contains redirections, they will be handled inside the child
 */
-void exec_one_cmd(char **cmd, char **env)
+int	exec_one_cmd(char **cmd, char **env, sem_t *sem)
 {
-    if (check_builtin)
-        return;
+	int	status;
+
+    //if (check_builtin(cmd, env))
+    //    return;
     int pid = fork();
     if (pid == -1)
-        algo de error;
-    if (!pid)
-        child(0, 1, cmd ,env);
+	{
+        printf("EXEC_ONE_CMD: fork fail\n");
+		return 1;
+	}
+	if (pid == 0)
+        child(0, 1, cmd, env, sem);
+	wait(&status);
+	return (status);
 }
 
-void exec_multi_cmd(t_list *cmds, char **env)
+int exec_multi_cmd(t_list *cmds, char **env, sem_t *sem)
 {
-	int i = 0;
+	int i;
 	int last_pipe[2];
 	int new_pipe[2];
 	int pid;
     int cmds_len;
+	int	status;
 
+	sem_wait(sem);
     cmds_len = ft_lstsize(cmds);
-	pipe(last_pipe);
+	if (pipe(last_pipe) == -1)
+	{
+		printf("EXEC_MULTI_CMD: pipe 0: fail\n");
+		return  1;
+	}
+	printf("EXEC_MULTI_CMD: pipe 0: read %d, write %d\n",last_pipe[0], last_pipe[1]);
 	pid = fork();
+	if (pid == -1)
+	{
+		printf("EXEC_MULTI_CMD: fork 0: fail\n");
+		return 1;
+	}
 	if (pid == 0)
 	{
 		close(last_pipe[0]);
-		child(0, last_pipe[1], (char **)cmds->content, path);
+		child(0, last_pipe[1], (char **)cmds->content, env, sem);
 	}
     cmds = cmds->next;
+	i = 0;
 	while (++i < cmds_len - 1)
 	{
-		pipe(new_pipe);
-		pid = fork();
-		if (pid == 0)
+		if (pipe(new_pipe) == -1)
 		{
-			close(last_pipe[1]);
+			printf("EXEC_MULTI_CMD: pipe %d fail\n",i);
+			return 1;
+		}
+		close(last_pipe[1]);
+		printf("EXEC_MULTI_CMD: pipe %d: read %d, write %d\n", i, new_pipe[0], new_pipe[1]);
+		pid = fork();
+		if (pid == -1)
+		{
+			printf("EXEC_MULTI_CMD: fork %d: fail\n", i);
+			return 1;
+		}
+		else if (pid == 0)
+		{
 			close(new_pipe[0]);
-			child(last_pipe[0], new_pipe[1], (char **)cmds->content, path);
+			child(last_pipe[0], new_pipe[1], (char **)cmds->content, env, sem);
 		}
 		else
 		{
 			close(last_pipe[0]);
-			close(last_pipe[1]);
 			last_pipe[0] = new_pipe[0];
 			last_pipe[1] = new_pipe[1];
 		}
         cmds = cmds->next;
 	}
-	pid = fork();
 	close(last_pipe[1]);
+	pid = fork();
 	if (pid == 0)
 	{
-		child(last_pipe[0], 1, (char **)cmds->content, path);
+		child(last_pipe[0], 1, (char **)cmds->content, env, sem);
 	}
 	close (last_pipe[0]);
-	while (wait(NULL) != -1)
+		sem_post(sem);
+	sem_close(sem);
+	while (wait(&status) != -1)
 	    ;
+	return (status);
 }
+
 /*  exec_cmd:
 		Executes each command from the "cmds" list.
 		If there is only one command, it will make a fork to execute it unless
@@ -98,19 +142,39 @@ void exec_multi_cmd(t_list *cmds, char **env)
 		[ ] If 1 cmd and is builtin, don't fork it
 		[ ] If 1 cmd and not builtin, fork the command
 		[ ] Execute builtins with multiple commands
+		[ ] return exit status on some variable
 		[ ] Pipes link between forks
 		[ ] Leaks
 		[ ] close pipes, files
 		[ ] norminette
 */
-void    exec_cmd(t_list *cmds, t_list *env)
+int    exec_cmd(t_list *cmds, t_list *env, sem_t *sem)
 {
     char    **env_array;
-    if (!cmds)
-        return ;
-    env_array = env_to_array(env);
+
+	if (!cmds)
+	{
+		printf("EXEC_CMD: no cmds\n");
+		return 1;
+	}
+    env_array = varlist_to_array(env);
     if (ft_lstsize(cmds) == 1)
-        exec_one_cmd((char **)cmds->content, env_array);
+        return (exec_one_cmd((char **)cmds->content, env_array,sem));
     else
-        exec_multi_cmd(cmds, env_array);
+        return (exec_multi_cmd(cmds, env_array,sem));
+}
+
+int main(int c, char **v, char **e)
+{
+	if (c == 1)
+		return (printf("1 arg\n"), 1);
+	sem_t *sem = sem_open("my_semaphore", O_CREAT, 0666, 1);
+    if (sem == SEM_FAILED) {
+        perror("sem_open");
+        return 1;
+    }
+	(void)c;
+	t_list *env = array_to_varlist(e);
+	t_list *cmd = parse(v[1], NULL, env);
+	return (exec_cmd(cmd, env,sem));
 }
