b568fab2d0f9a0e52770d8b0e22c409deafbf8cb
shuoscaryo <shuoscaryo@gmail.com>
2023-09-26 17:10:00 +0200

    Started a test for parse.c

---
+++ test/test_parse.c
@@ -0,0 +1,200 @@
+#include "libft.h"
+#include "t_var.h"
+#include <stdio.h>
+#include <sys/wait.h>
+
+t_list	*parse(char	*input, t_list *local, t_list *env);
+
+// Text attributes
+#define RESET "\x1b[0m"      // Reset all attributes to default
+#define BOLD "\x1b[1m"       // Bold or increased intensity
+#define NORMAL "\x1B[22m"	 // Normal width text
+#define DIM "\x1b[2m"        // Dim (decreased intensity)
+#define UNDERLINE "\x1b[4m"  // Underline
+#define BLINK "\x1b[5m"      // Blink
+#define REVERSE "\x1b[7m"    // Reverse (swap foreground and background colors)
+#define HIDDEN "\x1b[8m"     // Hidden (not widely supported)
+// Text colors
+#define BLACK "\x1b[30m"     	// Black text
+#define RED "\x1b[31m"       	// Red text
+#define GREEN "\x1b[32m"     	// Green text
+#define YELLOW "\x1b[33m"    	// Yellow text
+#define BLUE "\x1b[34m"      	// Blue text
+#define MAGENTA "\x1b[35m"   	// Magenta text
+#define CYAN "\x1b[36m"      	// Cyan text
+#define WHITE "\x1b[37m"     	// White text
+#define ORANGE "\x1b[38;5;208m"	// Orange text
+#define GREY "\x1B[90m"			// Grey text
+#define LIME "\x1B[92m"			// Lime text
+
+// Background colors
+#define BG_BLACK "\x1b[40m"  // Black background
+#define BG_RED "\x1b[41m"    // Red background
+#define BG_GREEN "\x1b[42m"  // Green background
+#define BG_YELLOW "\x1b[43m" // Yellow background
+#define BG_BLUE "\x1b[44m"   // Blue background
+#define BG_MAGENTA "\x1b[45m" // Magenta background
+#define BG_CYAN "\x1b[46m"   // Cyan background
+#define BG_WHITE "\x1b[47m"  // White background
+
+int test_number = 1;
+
+int	array_cmp(char **array1, char **array2)
+{
+	int i = 0;
+	while(array1[i] && array2[i])
+	{
+		if (ft_strcmp(array1[i],array2[i]))
+			return (0);
+		i++;
+	}
+	if (!array1[i] && array2[i])
+		return (0);
+	if (array1[i] && !array2[i])
+		return (0);
+	return (1);
+}
+
+int	same_result(t_list *result, t_list *expected)
+{
+	if (ft_lstsize(result) != ft_lstsize(expected))
+		return (0);
+	while(result)
+	{
+		if (!array_cmp(((char **)result->content), ((char **)expected->content)))
+			return (0);
+		result = result->next;
+		expected = expected->next;
+	}
+	return (1);
+}
+
+void print_list(t_list *list)
+{
+	int j = 0;
+	if (!list)
+		printf("\t\tcmd[0]: (NULL)\n");
+	for (t_list *aux = list; aux; aux = aux->next)
+	{
+		printf("\t\tcmd[%d]:\n", j);
+		int i;
+		for (i = 0; ((char **)aux->content)[i]; i++)
+			printf("\t\t\t%d.[%s]\n",i ,((char **)aux->content)[i]);
+		printf("\t\t\t%d.[%s]\n",i ,((char **)aux->content)[i]);
+		j++;
+	}
+}
+
+void print_error(t_list *expected, t_list *result)
+{
+
+}
+
+#define compare_result(result,expected) \
+	if (same_result(result, expected))\
+		printf(GREEN BOLD"%d.OK"RESET"\t"GREY"test:[%s]"RESET"\n",test_number++,input);\
+	else\
+	{\
+		printf(RED BOLD"%d.KO"RESET"\t"GREY"test:[%s]"RESET"\n",test_number++,input);\
+		printf(BLUE BOLD"\tExpected:\n"CYAN DIM);\
+		print_list(expected);\
+		printf(NORMAL GREEN BOLD"\tResult:\n"LIME DIM);\
+		print_list(result);\
+		printf(RESET NORMAL); \
+	}
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+void	test_NULL_input()
+{
+// INPUT VARIABLES
+	//set input string
+	char *input = NULL;
+	//set expected result with no mallocs so its easier to check
+	char *aux[]= {NULL};
+	t_list expected[1] = {{aux, NULL}};
+	//set local and env variables
+	t_list *local = NULL;
+	t_list *env = NULL;
+// EXECUTE TEST (DON'T TOUCH)
+	t_list *result = parse(input, local, env);
+	compare_result(result, expected)
+	ft_lstfree(result, ft_array_free);
+}
+void	test_empty_cmd()
+{
+// INPUT VARIABLES
+	//set input string
+	char *input = "";
+	//set expected result with no mallocs so its easier to check
+	char *aux[]= {"",NULL};
+	t_list expected[1] = {{aux, NULL}};
+	//set local and env variables
+	t_list *local = NULL;
+	t_list *env = NULL;
+// EXECUTE TEST (DON'T TOUCH)
+	t_list *result = parse(input, local, env);
+	compare_result(result, expected)
+	ft_lstfree(result, ft_array_free);
+}
+
+void	test_1arg_1cmd()
+{
+// INPUT VARIABLES
+	//set input string
+	char *input = "cat";
+	//set expected result with no mallocs so its easier to check
+	char *aux[]= {"cat",NULL};
+	t_list expected[1] = {{aux, NULL}};
+	//set local and env variables
+	t_list *local = NULL;
+	t_list *env = NULL;
+// EXECUTE TEST (DON'T TOUCH)
+	t_list *result = parse(input, local, env);
+	compare_result(result, expected)
+	ft_lstfree(result, ft_array_free);
+}
+
+void	test_1pipe()
+{
+// INPUT VARIABLES
+	//set input string
+	char *input = "cat Makefile | ls -la";
+	//set expected result with no mallocs so its easier to check
+	char *aux[]= {"cat", "Makefile", NULL};
+	char *aux2[]= {"ls", "-la", NULL};
+	t_list expected[2] = {{aux, &expected[1]}, {aux2, NULL}};
+	//set local and env variables
+	t_list *local = NULL;
+	t_list *env = NULL;
+// EXECUTE TEST (DON'T TOUCH)
+	//int pid = fork();
+	//if (pid == -1)
+	//{
+	//	perror("Fork_failed");
+	//	return ;
+//	}
+	//if (pid == 0)
+	//{
+		t_list *result = parse(input, local, env);
+		compare_result(result, expected)
+		ft_lstfree(result, ft_array_free);
+		exit (0);
+	//}
+	//int status;
+	//wait(&status);
+	//if (WIFEXITED(status))
+
+}
+
+int main(int c, char **v, char **env_in)
+{
+	printf(BOLD UNDERLINE MAGENTA"TESTER: PARSE"RESET"\n");
+	test_NULL_input();
+	test_empty_cmd();
+	test_1arg_1cmd();
+	test_1pipe();
+}
+//gcc -fsanitize=leak -Iinclude -Ilibft/include src/parse.c src/expand_variables.c src/split* test/test_parse.c src/print_varlist.c src/array_to_varlist.c src/set_variable.c src/free_var.c libft/libft.a
