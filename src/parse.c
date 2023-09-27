188c819054e58d89dc0a1da6fc2153602c059d48
shuoscaryo <shuoscaryo@gmail.com>
2023-09-27 21:02:13 +0200

    quicksave

--- src/parse.c
+++ src/parse.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_parse.c                                         :+:      :+:    :+:   */
+/*   parse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: orudek <orudek@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 17:24:03 by orudek            #+#    #+#             */
-/*   Updated: 2023/09/19 20:51:17 by orudek           ###   ########.fr       */
+/*   Updated: 2023/09/26 23:46:06 by orudek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,6 +67,8 @@ t_list	*parse(char	*input, t_list *local, t_list *env)
 	char	**expanded;
 	t_list	*out;
 
+	if (!input)
+		return (NULL);
 	split_pipes = split_pipe(input);
 	if (!split_pipes)
 		return (NULL);
@@ -78,63 +80,3 @@ t_list	*parse(char	*input, t_list *local, t_list *env)
 	ft_array_free(expanded);
 	return (out);
 }
-
-//COMENTAR A PARTIR DE AQUI
-/*
-#include "t_var.h"
-int save_env(t_list **list, char **envp)
-{
-	char *name;
-	char *content;
-	int	len;
-	int	i;
-	int j;
-	int aux;
-
-	i = 0;
-	while (envp[i])
-	{
-		j = 0;
-		len = 0;
-		while (envp[i][j] != '=')
-		{
-			len++;
-			j++;
-		}
-		name = malloc(sizeof(char) * (len + 1));
-		ft_strlcpy(name, envp[i], len + 1);
-		j++;
-		aux = j;
-		len = 0;
-		while (envp[i][j] != '\0')
-		{
-			len++;
-			j++;
-		}
-		content = malloc(sizeof(char) * (len + 1));
-		ft_strlcpy(content, &envp[i][aux], len + 1);
-		set_variable(list, name, content);
-		i++;
-	}
-	return (0);
-}
-int main (int c, char **v, char **env)
-{
-	if (c == 1)
-		return 1;
-	t_list	*env_list;
-	save_env(&env_list, env);
-	t_list *parsed = parse(v[1], NULL, env_list);
-	t_list *aux = parsed;
-	int j = 0;
-	while (aux)
-	{
-		printf("List %d\n",j++);
-		int i = 0;
-		for (i = 0; ((char **)aux->content)[i]; i++)
-			printf("\tstr[%d]=%s$\n",i,((char **)aux->content)[i]);
-		printf("\tstr[%d]=%s$\n",i,((char **)aux->content)[i]);
-		aux = aux->next;
-	}
-	exit (1);
-}*/
