c81fc7e1c33b49dc9a6c183d816feb9d3c082c79
shuoscaryo <shuoscaryo@gmail.com>
2023-09-28 18:30:23 +0200

    improved parse_test script so it allows flags

--- test/parse_test.sh
+++ test/parse_test.sh
@@ -1,1 +1,29 @@
-gcc -fsanitize=leak -Iinclude -Ilibft/include src/parse.c src/expand_variables.c src/split* test/test_parse.c src/print_varlist.c src/array_to_varlist.c src/set_variable.c src/free_var.c libft/libft.a -o parse_test && ./parse_test -err; rm -rf parse_test
+#!/bin/bash
+
+if [ $# -ge 1 ]; then
+	if [ $1 = "-err" ]; then
+		echo -e "\033[0;36mExecuting parse_test with -err flag\033[0m"
+		flag=$1
+	fi
+	if [ $1 = "-all" ]; then
+		echo -e "\033[0;36mExecuting parse_test with -all flag\033[0m"
+		flag=$1
+	fi
+fi
+
+make -C libft/ >/dev/null
+if [ $? -ne 0 ]; then
+	echo -e "\033[0;31mError: libft compilation failed\033[0m"
+	exit 1
+fi
+gcc -fsanitize=leak \
+	-Iinclude -Ilibft/include \
+	src/parse/*.c src/variables/*.c test/test_parse.c \
+	libft/libft.a \
+	-o parse_test
+if [ $? -ne 0 ]; then
+	echo -e "\033[0;31mError: parse_test compilation failed\033[0m"
+	exit 1
+fi
+./parse_test $flag
+rm -rf parse_test
