be7dc4058426db1ddd6554e8415bddd96a3661b4
shuoscaryo <shuoscaryo@gmail.com>
2023-09-28 18:28:23 +0200

    .

--- include/t_var.h
+++ include/t_var.h
@@ -6,7 +6,7 @@
 /*   By: orudek <orudek@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 13:20:58 by orudek            #+#    #+#             */
-/*   Updated: 2023/09/27 22:49:08 by orudek           ###   ########.fr       */
+/*   Updated: 2023/09/28 18:10:58 by orudek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,32 @@
 
 # include "libft.h"
 
-typedef struct s_variable
+# define DEFAULT_VAR 0
+# define LOCAL_VAR 1
+# define ENV_VAR 2
+
+typedef struct s_var
 {
 	char	*name;
 	char	*content;
+	int		type;
 }	t_var;
 
-char	set_variable(t_list **lst, char *name, char *content);
-int		get_variable(t_list *lst, char *name, char **content);
-void	unset_variable(t_list **lst, char *name);
 t_list	*array_to_varlist(char **array);
 char	**varlist_to_array(t_list *lst, int ignore_null);
+
+t_var	*new_var(char *name, char *content, int	type);
+t_var	*dup_var(t_var *var);
 void	free_var(void *var);
-void	print_varlist(t_list *varlist); //XXX delete this before submitting project
+
+int		set_variable(t_list **lst, char *name, char *content, int type);
+int		set_variable2(t_list **lst, t_var *var);
+int		get_variable(t_list *lst, char *name, t_var **var);
+void	unset_variable(t_list **lst, char *name);
+
+int		is_in_varlist(t_list *lst, char *name);
 int		is_varname_ok(const char *name);
+
+void	print_varlist(t_list *varlist); //XXX delete this before submitting project
 
 #endif
