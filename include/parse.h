3ed482328c8ad7c6457a57ca3678267a61939fc3
shuoscaryo <shuoscaryo@gmail.com>
2023-10-03 12:49:01 +0200

    created structures for parsing command

--- include/parse.h
+++ include/parse.h
@@ -6,7 +6,7 @@
 /*   By: orudek <orudek@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 13:58:18 by orudek            #+#    #+#             */
-/*   Updated: 2023/09/29 14:00:11 by orudek           ###   ########.fr       */
+/*   Updated: 2023/10/03 12:47:10 by orudek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,19 @@
 # define PARSE_H
 
 # include "libft.h"
+
+typedef struct s_arg_redir
+{
+	t_list *args;
+	t_list *redir;
+} t_arg_redir;
+
+typedef struct s_cmd
+{
+	char **args;
+	int	infile;
+	int outfile;
+} t_cmd;
 
 char	**split_pipe(char *s);
 char	*expand_variables(char *str, t_list *varlist);
