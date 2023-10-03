111067400b0822e0e2c2865683aff23e33d3039f
NACHORTE <ignaciortegape@gmail.com>
2023-10-03 13:38:33 +0200

    quotes

--- src/parse/parse.c
+++ src/parse/parse.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: orudek <orudek@student.42madrid.com>       +#+  +:+       +#+        */
+/*   By: iortega- <iortega-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 17:24:03 by orudek            #+#    #+#             */
-/*   Updated: 2023/09/29 19:26:14 by orudek           ###   ########.fr       */
+/*   Updated: 2023/10/03 13:28:24 by iortega-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,6 +55,58 @@ t_list	*save_commands(char **array)
 		array++;
 	}
 	return (output);
+}
+
+t_arg_redir	*arg_redir(char **cmds)
+{
+	t_arg_redir	*out;
+	int	i;
+	int	j;
+	int	quotes;
+	int	simples;
+	char	*str;
+
+	out = malloc(sizeof(t_arg_redir));
+	if (!out)
+		return (NULL);
+	i = 0;
+	quotes = 0;
+	simples = 0;
+	str = NULL;
+	while (cmds[i])
+	{
+		j = 0;
+		while (cmds[i][j])
+		{
+			if (cmds[i][j] == '\'' && !quotes)
+				simples = !simples;
+			else if (cmds[i][j] == '"' && !simples)
+				quotes = !quotes;
+			if (!quotes && !simples)
+			{
+				if (cmds[i][j] == '<' || cmds[i][j] == '>')
+				{
+					while (cmds[i][j] == ' ')
+						j++;
+					while (cmds[i][j] && cmds[i][j] != ' ')
+					{
+						if (cmds[i][j] == '\'')
+						{
+							while (cmds[i][j] != '\'')
+							{
+								
+							}
+						}
+						else if (cmds[i][j] == '"')
+						{
+							
+						}
+						
+					}
+				}
+			}
+		}
+	}
 }
 
 t_list	*parse(char	*input, t_list *varlist)
